8e186a7693e7c1e3f322947c33278e82e7b3e83d
Viktik <viktor.askor@gmail.com>
2018-07-06 12:44:55 +0300

    test

--- test.php
+++ test.php
@@ -1,23 +1,26 @@
 <?php
 
-$fruits = array("fruits" => array("a" => "апельсин",
-    "b" => "банан",
-    "c" => "яблоко"
-),
-    "numbers" => array(1,
-        2,
-        3,
-        4,
-        5,
-        6
-    ),
-    "holes" => array("первая",
-        5 => "вторая",
-        "третья"
-    )
+$fruits = array("fruits" => array(
+                                "a" => "апельсин",
+                                "b" => "банан",
+                                "cola" => array(
+                                                "яблоко" => "good",
+                                                "cola" => "bad")
+                                ),
+                "numbers" => array(1,
+                                    2,
+                                    3,
+                                    4,
+                                    5,
+                                    6
+                ),
+                "holes" => array("первая",
+                                5 => "вторая",
+                                "третья"
+                )
 );
 $sorting = function ($item, $key) {
-    if ($key == 'a') {
+    if ($key == 'яблоко') {
         return true;
     }
 };
