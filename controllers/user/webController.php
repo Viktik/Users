7b1d06ad44f2a50c8d89a66a432d97d28ff1128e
Viktik <viktor.askor@gmail.com>
2018-07-10 17:56:45 +0300

    webController start

--- controllers/user/webController.php
+++ controllers/user/webController.php
@@ -1,1 +1,14 @@
 <?php
+require_once "../../vendor/autoload.php";
+require "../../config.php";
+
+use classes\IUser\IUser, classes\UserJson\UserJson, classes\UserSQL\UserSQL;
+
+switch ($base) {
+    case 'json':
+        $user = new UserJson();
+        break;
+    case 'sql':
+        $user = new UserSQL();
+        break;
+}
