9d4782b6b396dfb487e802e17ca1755205303086
Viktik <viktor.askor@gmail.com>
2018-07-13 18:05:21 +0300

    some fixes

--- actions/saveUser.php
+++ actions/saveUser.php
@@ -10,15 +10,15 @@ $phone = $controller->clearStr($_POST['phone']);
 $email = $controller->clearStr($_POST['email']);
 
 if (empty($name) || empty($phone) || empty($email)) {
-    echo "Заполните все поля формы!<br/>";
-    echo "<a href='../htmlForms/newUserForm.html'>Вернуться к заполнению</a><br/>";
-    echo "<a href='../index.php'>К списку</a>";
-}else{
-    if (!$controller->addNewUser($name,$phone,$email)){
-        echo "Ошибка при добавлении пользователя<br/>";
-        echo "<a href='../index.php'>Вернуться к списку</a>";
-    }else{
-        echo "Пользователь успешно добавлен!<br/>";
-        echo "<a href='../index.php'>Вернуться к списку</a>";
-    }
+        echo "Заполните все поля формы!<br/>";
+        echo "<a href='../htmlForms/newUserForm.html'>Вернуться к заполнению</a><br/>";
+        echo "<a href='../index.php'>К списку</a>";
+    } else {
+        if (!$controller->addNewUser($name, $phone, $email)) {
+            echo "Ошибка при добавлении пользователя<br/>";
+            echo "<a href='../index.php'>Вернуться к списку</a>";
+        } else {
+            echo "Пользователь успешно добавлен!<br/>";
+            echo "<a href='../index.php'>Вернуться к списку</a>";
+        }
 }
--- actions/updateUser.php
+++ actions/updateUser.php
@@ -13,11 +13,11 @@ $email = $controller->clearStr($_POST['email']);
 if (empty($name) || empty($phone) || empty($email)) {
     echo "Ошибка! Заполните все поля формы!<br/>";
     echo "<a href='../index.php'>К списку</a>";
-}else{
-    if (!$result = $controller->updateUser($oldEmail,$name,$phone,$email)){
+} else {
+    if (!$result = $controller->updateUser($oldEmail, $name, $phone, $email)) {
         echo "Ошибка при редактировании пользователя.<br/>";
         echo "<a href='../index.php'>Вернуться к списку</a>";
-    }else{
+    } else {
         echo "Пользователь успешно отредактирован.<br/>";
         echo "<a href='../index.php'>Вернуться к списку</a>";
     }
